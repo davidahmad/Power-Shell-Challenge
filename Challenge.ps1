cdee2b0b4419c636d4f20cbc40dcf8b5134cdf4f
David Ahmad <davidfahmad@gmail.com>
2020-10-03 22:12:41 -0700

    PS script

---
+++ Challenge.ps1
@@ -0,0 +1,29 @@
+﻿ Param(
+   [Parameter(Mandatory=$true)]
+   [string]$foo
+)
+
+#Returns the first recurring character
+function FirstRecurringChar($InputString){
+
+#convert string to character array
+$arrayOfChars = $InputString.ToCharArray()
+
+#initialize hash table
+$hashRepeat = @{}
+
+#loop through all characters in the array
+foreach($char in $arrayOfChars){
+    #Check if character exists in hash table, if it does then
+    #that means this is the second occurence of the character
+    if($hashRepeat.Contains($char)){
+        return $char
+    }
+    #Add character to hash table if character doesn't already exist.
+    $hashRepeat += @{$char = 1}
+}
+}
+
+
+$FRC = FirstRecurringChar($foo)
+Write-Host $FRC
